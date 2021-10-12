152a83d2abcd5653d2e256e4cea4aff582548e50
elogeek <elodiechristin@gmail.com>
2021-10-12 17:17:32 +0200

    start manager in the forum

--- Model/Repository/CategoryRepository.php
+++ Model/Repository/CategoryRepository.php
@@ -1,1 +1,84 @@
 <?php
+namespace App\Repository;
+
+use App\Model\Entity\Category;
+use DB;
+use PDO;
+use PDOException;
+
+class CategoryRepository {
+
+    /**
+     * Get all categories.
+     * @return array
+     */
+    public function getCategory(): array {
+        $cat = [];
+        $request = DB::getInstance()->prepare("SELECT * FROM category");
+        if ($request->execute() && $data = $request->fetchAll()) {
+            foreach ($data as $cData) {
+                $cat[] = new Category($cData['id'], $cData['name']);
+            }
+        }
+        return $cat;
+    }
+
+    /**
+     * Return a category based on a given role id.
+     * @param int $id
+     * @return Category|null
+     */
+    public function getCatById(int $id): ?Category {
+        $request = DB::getInstance()->prepare("SELECT * FROM category WHERE id = :id");
+        $request->bindValue(':id', $id);
+        $result = $request->execute();
+        if ($result && $data = $request->fetch()) {
+            return new Category($data['id'], $data['name']);
+        }
+
+        return null;
+    }
+
+    /**
+     * Return a category by name
+     * @param string $catName
+     * @return Category
+     */
+    public function getCatByName(string $catName): Category {
+        $request = DB::getInstance()->prepare("SELECT * FROM category WHERE name = :name");
+        $request->bindValue(':name', $catName);
+        $request->execute();
+        $cat = new Category();
+        if ($data = $request->fetch()) {
+            $cat->setId($data['id']);
+            $cat->setName($data['name']);
+        }
+
+        return $cat;
+    }
+
+    /**
+     * Add a new category
+     * @param Category $cat
+     * @return bool
+     */
+    public function addCat(Category $cat): bool {
+        $request = DB::getInstance()->prepare("INSERT INTO category (name) VALUES (:name)");
+        $request->bindValue(':name', $cat->getName());
+        $request->execute();
+        $cat->setId(DB::getInstance()->lastInsertId());
+        return $cat->getId() !== null && $cat->getId() > 0;
+    }
+
+    /**
+     * Delete a category
+     * @param Category $cat
+     * @return bool
+     */
+    public function deleteCat(Category $cat): bool {
+        $request = DB::getInstance()->prepare("DELETE FROM category WHERE id = :id");
+        $request->bindValue('id', $cat->getId());
+        return $request->execute();
+    }
+
+}
--- Model/Repository/RoleRepository.php
+++ Model/Repository/RoleRepository.php
@@ -1,1 +1,87 @@
 <?php
+namespace App\Repository;
+
+use App\Model\Entity\Role;
+use DB;
+use PDO;
+use PDOException;
+
+/**
+ * Class RoleRepository
+ */
+class RoleRepository {
+
+    /**
+     * Get all rôles.
+     * @return array
+     */
+    public function getRoles(): array {
+        $roles = [];
+        $request = DB::getInstance()->prepare("SELECT * FROM role");
+        if ($request->execute() && $data = $request->fetchAll()) {
+            foreach ($data as $roleData) {
+                $roles[] = new Role($roleData['id'], $roleData['name']);
+            }
+        }
+        return $roles;
+    }
+
+    /**
+     * Return a Role object based on a given role id.
+     * @param int $id
+     * @return Role|null
+     */
+    public function getRoleById(int $id): ?Role {
+        $request = DB::getInstance()->prepare("SELECT * FROM role WHERE id = :id");
+        $request->bindValue(':id', $id);
+        $result = $request->execute();
+        if ($result && $data = $request->fetch()) {
+            return new Role($data['id'], $data['name']);
+        }
+
+        return null;
+    }
+
+    /**
+     * Return a Role by name
+     * @param string $roleName
+     * @return Role
+     */
+    public function getRoleByName(string $roleName): ?Role {
+        $request = DB::getInstance()->prepare("SELECT * FROM role WHERE name = :name");
+        $request->bindValue(':name', $roleName);
+        $request->execute();
+        $role = new Role();
+        if ($rdata = $request->fetch()) {
+            $role->setId($rdata['id']);
+            $role->setName($rdata['name']);
+        }
+
+        return $role;
+    }
+
+    /**
+     * Add a new role
+     * @param Role $role
+     * @return bool
+     */
+    public function addRole(Role &$role): bool {
+        $request = DB::getInstance()->prepare("INSERT INTO role (name) VALUES (:role)");
+        $request->bindValue(':role', $role->getName());
+        $request->execute();
+        $role->setId(DB::getInstance()->lastInsertId());
+        return $role->getId() !== null && $role->getId() > 0;
+    }
+
+    /**
+     * Delete a role
+     * @param Role $role
+     * @return bool
+     */
+    public function deleteRole(Role $role): bool {
+        $request = DB::getInstance()->prepare("DELETE FROM role WHERE id = :id");
+        $request->bindValue('id', $role->getId());
+        return $request->execute();
+    }
+
+}
