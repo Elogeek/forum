f051a29e4a9ad041547e860f09392729238e814b
elogeek <elodiechristin@gmail.com>
2021-10-15 17:33:41 +0200

    suite manager in the forum

--- Model/Repository/SubjectRepository.php
+++ Model/Repository/SubjectRepository.php
@@ -15,12 +15,30 @@ class SubjectRepository {
         $request = DB::getInstance()->prepare("SELECT * FROM subject");
         if ($request->execute() && $data = $request->fetchAll()) {
             foreach ($data as $sData) {
-                $x[] = new Subject($sData['id'],$sData['title'],$sData['content'], $sData['categoryFk'], $sData['auhorfK'],$sData['commentFk'], $sData['subject'],$sData['statusFk']);
+                $x[] = new Subject($sData['id'],$sData['title'],$sData['content'], $sData['categoryFk'], $sData['auhorfK'],$sData['commentFk'], $sData['subject'],$sData['subjectStatFk']);
             }
         }
         return $x;
     }
 
-    pu
+    /**
+     * @param Subject $a
+     * @return bool
+     */
+    public function addSubject(Subject $a): bool {
+        $request = DB::getInstance()->prepare(
+            "INSERT INTO subject(title,content,categoryFk,authorFk,commentFk,subjectStatFk) 
+                    VALUES(:t,:content,:catFk,:authorFk,:commentFk,:subjStatFk)"
+        );
+        $request->bindValue(':t', $a->getTitle());
+        $request->bindValue(':content', $a->getContent());
+        $request->bindValue(':catFk', $a->getCategoryFk());
+        $request->bindValue(':authorFk', $a->getAuthorFk());
+        $request->bindValue(':commentFk', $a->getCommentFk());
+        $request->bindValue(':subjectStatFk', $a->getSubjectStatFk());
+
+        $a->setId(DB::getInstance()->lastInsertId());
+        return $a->getId() !== null && $a->getId() > 0;
+    }
 
 }
