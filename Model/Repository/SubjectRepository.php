fbd0861f8ef3864a9a6e04dd172d2f9343a0c726
elogeek <elodiechristin@gmail.com>
2021-10-15 17:18:49 +0200

    suite manager in the forum

--- Model/Repository/SubjectRepository.php
+++ Model/Repository/SubjectRepository.php
@@ -1,8 +1,26 @@
 <?php
 namespace App\Repository;
 
+use App\Model\Entity\Subject;
 use DB;
 
 class SubjectRepository {
+
+    /**
+     * Get all subjects.
+     * @return array
+     */
+    public function getSubject(): array {
+        $x = [];
+        $request = DB::getInstance()->prepare("SELECT * FROM subject");
+        if ($request->execute() && $data = $request->fetchAll()) {
+            foreach ($data as $sData) {
+                $x[] = new Subject($sData['id'],$sData['title'],$sData['content'], $sData['categoryFk'], $sData['auhorfK'],$sData['commentFk'], $sData['subject'],$sData['statusFk']);
+            }
+        }
+        return $x;
+    }
+
+    pu
 
 }
