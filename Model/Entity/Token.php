cd9a32af5abba669c70e8b4698f8dfe653336b78
elogeek <elodiechristin@gmail.com>
2022-08-29 14:05:12 +0200

    Minor refresh entity

--- Model/Entity/Token.php
+++ Model/Entity/Token.php
@@ -16,4 +16,53 @@ class Token {
         $this->userFk = $userFk;
         $this->token = $token;
     }
+
+    /**
+     * Get id of the token
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get user of  the token
+     * @return int|null
+     */
+    public function getUserFk(): ?int
+    {
+        return $this->userFk;
+    }
+
+    /**
+     * Set user of  the token
+     * @param $userFk
+     * @return Token
+     */
+    public function setUserFk($userFk): Token
+    {
+        $this->userFk = $userFk;
+        return $this;
+    }
+
+    /**
+     * Get token of the token
+     * @return string|null
+     */
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set token of the token
+     * @param string|null $token
+     * @return Token
+     */
+    public function setToken(?string $token): Token
+    {
+        $this->token = $token;
+        return $this;
+    }
 }
